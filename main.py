abf2bfe463bf54c45548b6a3b4452cce57b57515
j-lepard <jamie@lepardandlepard.com>
2023-11-05 16:00:40 -0800

    Sigh - trying only 1 category

--- main.py
+++ main.py
@@ -1,30 +1,8 @@
-from dash import Dash, dcc, html, Input, Output
-import plotly.graph_objects as go
+from xgboost import XGBRegressor
 
-app = Dash(__name__)
+# Create an XGBoost regressor
+xgb_regressor = XGBRegressor(tree_method='gpu_hist')
 
+# Check if GPU support is available
+print(f"Is GPU available: {xgb_regressor._is_gpu_available()}")
 
-app.layout = html.Div([
-    html.H4('Interactive color selection with simple Dash example'),
-    html.P("Select color:"),
-    dcc.Dropdown(
-        id="dropdown",
-        options=['Gold', 'MediumTurquoise', 'LightGreen'],
-        value='Gold',
-        clearable=False,
-    ),
-    dcc.Graph(id="graph"),
-])
-
-
-@app.callback(
-    Output("graph", "figure"), 
-    Input("dropdown", "value"))
-def display_color(color):
-    fig = go.Figure(
-        data=go.Bar(y=[2, 3, 1], # replace with your own data source
-                    marker_color=color))
-    return fig
-
-
-app.run_server(debug=True)
